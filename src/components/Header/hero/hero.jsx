0160ebe43f1c39cac595001f3db4bcd48e27019d
vikash jha <vikashjha@vikashs-MacBook-Air.local>
2023-01-25 23:02:05 +0530

    hero component done

--- src/components/Header/hero/hero.jsx
+++ src/components/Header/hero/hero.jsx
@@ -1,25 +1,59 @@
-import React from 'react'
-import css from './Hero.module.scss'
+import React from "react";
+import { fadeIn, slideIn, staggerContainer } from "../../../utils/motion";
+import css from "./Hero.module.scss";
+import { motion } from "framer-motion";
 const Hero = () => {
-    return (
-        <section className={`paddings ${css.wrapper}`}>
-            <div className={`innerWidth ${css.container}`}>
-                <div className={`${css.upperElements}`}></div>
-                <span>Hey There, <br/> I'm Vikash Jha</span>
-                <span>I design beautiful simple <br/> things, And i Love i do</span>
-                <div className={`${css.lowerElements}`}></div>
-                <div className={`${css.experience}`}>
-                    <div className='primaryText'>10</div>
-                    <div className='secondaryText'>
-                        <div>
-                            
-                        </div>
-                    </div>
-                </div>
-                <div className={`${css.certificate}`}></div>
-            </div>
-        </section>
-    )
-}
+  return (
+    <section className={`paddings ${css.wrapper}`}>
+      <motion.div
+        variants={staggerContainer}
+        initial="hidden"
+        whileInView="show"
+        viewport={{ once: false, amount: 0.25 }}
+        className={`innerWidth ${css.container}`}
+      >
+        <div className={css.upperElements}>
+          <motion.span className="primaryText" variants={fadeIn("right", "tween", 0.2, 1)}>
+            Hey There,
+            <br />
+            I'm Vikash.
+          </motion.span>
+          <motion.span className="secondaryText"variants={fadeIn("left", "tween", 0.4, 1)}>
+            I design beautiful simple
+            <br />
+            things, And I love what i do{" "}
+          </motion.span>
+        </div>
 
-export default Hero
+        <motion.div
+          variants={fadeIn("up", "tween", 0.3, 1)}
+          className={css.person}
+        >
+          <motion.img variants={slideIn("up", "tween", 0.5, 1.3)} src="./person.png" alt="" />
+        </motion.div>
+
+        <a className={css.email} href="mailto:vikashjha125@gmail.com">
+          vikashjha125@gmail.com
+        </a>
+
+        <div className={css.lowerElements}>
+          <motion.div variants={fadeIn("right", "tween", 0.3, 1)} className={css.experience}>
+            <div className="primaryText">02</div>
+            <div className="secondaryText">
+              <div>Years</div>
+              <div>Experience</div>
+            </div>
+          </motion.div>
+
+          <motion.div variants={fadeIn("left", "tween", 0.5, 1)} className={css.certificate}>
+            <img src="./certificate.png" alt="" />
+            <span>CERTIFIED PROFATIONAL</span>
+            <span>UI/UX DESIGNER</span>
+          </motion.div>
+        </div>
+      </motion.div>
+    </section>
+  );
+};
+
+export default Hero;
